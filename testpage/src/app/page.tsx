eca7d3b49afe2bfbff0eb4a14c31d521de84eff6
unknown <s1110078@student.hsleiden.nl>
2024-01-31 03:11:30 +0100

    Second commit, folders didn't upload

---
+++ testpage/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+import homeImg from 'public/home.jpg';
+import Hero from '@/components/hero';
+
+export default function Home() {
+  return (
+    <Hero
+      imgData={homeImg}
+      imgAlt="car factory"
+      title="Professional Cloud Hosting"
+    />
+  );
+}
